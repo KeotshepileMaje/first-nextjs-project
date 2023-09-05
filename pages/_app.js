0ee33f0c0a6af966d9f17748aabcb80b195dbf7c
keotshepileMaje <keotshepilemaje@gmail.com>
2023-09-05 15:40:23 +0200

    Added main pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
+import Layout from '@/components/layout/Layout'
 import '../styles/globals.css'
 
 export default function MyApp ({Component, pageProps}) {
+    
 
     return (
-        <Component {...pageProps} />
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
     )
 }
