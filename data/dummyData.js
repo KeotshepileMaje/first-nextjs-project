8a1715fe0591b3d4f03b8bbd75fcfd1c65ffc714
keotshepileMaje <keotshepilemaje@gmail.com>
2023-09-11 16:28:10 +0200

    Commenting out the the local dummy data

--- data/dummyData.js
+++ data/dummyData.js
@@ -31,13 +31,13 @@ const DUMMY_EVENTS = [
     },
   ];
   
-  export function getFeaturedEvents() {
-    return DUMMY_EVENTS.filter((event) => event.isFeatured);
-  }
+  // export function getFeaturedEvents() {
+  //   return DUMMY_EVENTS.filter((event) => event.isFeatured);
+  // }
   
-export function getAllEvents() {
-    return DUMMY_EVENTS;
-}
+// export function getAllEvents() {
+//     return DUMMY_EVENTS;
+// }
 
 export function getFilteredEvents(dateFilter) {
     const { year, month } = dateFilter;
@@ -50,6 +50,6 @@ export function getFilteredEvents(dateFilter) {
     return filteredEvents;
 }
 
-export function getEventById(id) {
-    return DUMMY_EVENTS.find((event) => event.id === id);
-}
+// export function getEventById(id) {
+//     return DUMMY_EVENTS.find((event) => event.id === id);
+// }
