f713011cc5a88ca08f96a3aa8f9b8fa851b16a53
keotshepileMaje <keotshepilemaje@gmail.com>
2023-09-14 16:27:52 +0200

    Added comments

--- components/input/comment-list.js
+++ components/input/comment-list.js
@@ -1,18 +1,17 @@
 import classes from './comment-list.module.css';
 
 function CommentList(props) {
-  const { items } = props;
+  const {items} =props
+
+  if (!items) {
+    return <div>Loading...</div>
+  }
 
   return (
     <ul className={classes.comments}>
-      {items.map((item) => (
-        <li key={item._id}>
-          <p>{item.text}</p>
-          <div>
-            By <address>{item.name}</address>
-          </div>
-        </li>
-      ))}
+      {items.map(
+        (item)=> <li key={item.id}>{item.text}</li>
+      )}
     </ul>
   );
 }
--- components/input/comments.js
+++ components/input/comments.js
@@ -1,72 +1,41 @@
-import { useContext, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import CommentList from './comment-list';
 import NewComment from './new-comment';
 import classes from './comments.module.css';
-import NotificationContext from '../../store/notification-context';
 
 function Comments(props) {
   const { eventId } = props;
 
-  const notificationCtx = useContext(NotificationContext);
-
   const [showComments, setShowComments] = useState(false);
-  const [comments, setComments] = useState([]);
-  const [isFetchingComments, setIsFetchingComments] = useState(false);
+  const [comments, setComments] = useState([])
+  console.log(comments)
 
-  useEffect(() => {
-    if (showComments) {
-      setIsFetchingComments(true);
+  
+  useEffect(
+    () => {
       fetch('/api/comments/' + eventId)
-        .then((response) => response.json())
-        .then((data) => {
-          setComments(data.comments);
-          setIsFetchingComments(false);
-        });
-    }
-  }, [showComments]);
+        .then(res => res.json())
+        .then( data => setComments(data.comments))
+    },[comments]
+  )
+  console.log(comments)
 
   function toggleCommentsHandler() {
     setShowComments((prevStatus) => !prevStatus);
   }
 
   function addCommentHandler(commentData) {
-    notificationCtx.showNotification({
-      title: 'Sending comment...',
-      message: 'Your comment is currently being stored into a database.',
-      status: 'pending',
-    });
-
-    fetch('/api/comments/' + eventId, {
+    // send data to API
+    fetch(`/api/comments/${eventId}`, {
       method: 'POST',
       body: JSON.stringify(commentData),
       headers: {
-        'Content-Type': 'application/json',
-      },
+        'Content-Type': 'application/json'
+      }
     })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-
-        return response.json().then((data) => {
-          throw new Error(data.message || 'Something went wrong!');
-        });
-      })
-      .then((data) => {
-        notificationCtx.showNotification({
-          title: 'Success!',
-          message: 'Your comment was saved!',
-          status: 'success',
-        });
-      })
-      .catch((error) => {
-        notificationCtx.showNotification({
-          title: 'Error!',
-          message: error.message || 'Something went wrong!',
-          status: 'error',
-        });
-      });
+      .then( res => res.json())
+      .then( data => console.log(data))
   }
 
   return (
@@ -75,8 +44,7 @@ function Comments(props) {
         {showComments ? 'Hide' : 'Show'} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && !isFetchingComments && <CommentList items={comments} />}
-      {showComments && isFetchingComments && <p>Loading...</p>}
+      {showComments && <CommentList items={comments} />}
     </section>
   );
 }
